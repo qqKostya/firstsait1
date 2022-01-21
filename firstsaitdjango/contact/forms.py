53b8f02532a0363ae969019849c0a7526e25e297
Kostya <leap74rus@gmail.com>
2022-01-21 19:57:54 +0300

    Lesson #1

---
+++ firstsaitdjango/contact/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Contact
+
+class ContactForm(forms.ModelForm):
+    """Форма подписки на email"""
+    class Meta:
+        model = Contact
+        fields = ('email',)
+        widgets = {
+            "email": forms.TextInput(attrs={"class": "editContent", "placeholder": "Your Email..."})
+        }
+        labels = {
+            "email": ''
+        }
